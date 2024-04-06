0ec75275d318fc06c4236ce5f3072218a4748b12
YJ9352 <act07612@gmail.com>
2024-04-07 00:26:37 +0900

    feat: 세팅

---
+++ src/main/kotlin/com/oauth2study/oauth_login_study/OauthLoginStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.oauth2study.oauth_login_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthLoginStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<OauthLoginStudyApplication>(*args)
+}
